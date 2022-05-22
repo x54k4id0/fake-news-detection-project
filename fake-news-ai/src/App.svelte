7780981143b0bc72db9faa23de5ee5d79c6bf50c
aymouune <79173423+aymouune@users.noreply.github.com>
2022-05-22 02:01:58 +0100

    Feat: Main jsx code - Entries, Labels and Buttons

--- fake-news-ai/src/App.svelte
+++ fake-news-ai/src/App.svelte
@@ -33,7 +33,40 @@
   };
 </script>
 
-<main>Hello</main>
+<main>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="main-verification-input-wrap">
+        <ul>
+          <li>
+            In order to verify your news we offer you this simple AI. The AI
+            will respond by <span style="color:#DA2C43; font-size:18px;">Fake</span> or
+            <span style="color:#16d509f2; font-size:18px;">Real</span>.
+          </li>
+          <li>Enter that news below.</li>
+        </ul>
+        <div class="main-verification-input fl-wrap">
+          <div class="main-verification-input-item">
+            <input
+              type="text"
+              bind:value={news}
+              placeholder="Enter your news here..."
+            />
+          </div>
+          <button class="main-verification-button" on:click={getAnswer}>Verify Now</button>
+        </div>
+
+        <div class="answer" style="visibility:{visibility}">
+			  <h2>Your news is <span style="color:indigo">{percent}</span> : </h2>
+          	<h1 style="color:{color}">{answer}</h1>
+        </div>
+      </div>
+    </div>
+    <div style="visibility: {loading};">
+      <Stretch size="60" color="#FFFFFF" unit="px" duration="1s"></Stretch>
+    </div>
+  </div>
+</main>
 
 <style>
 </style>
