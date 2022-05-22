4a9b3ca504dbc6e213f691ea09cfb3dc99cf67cd
aymouune <79173423+aymouune@users.noreply.github.com>
2022-05-22 01:59:57 +0100

    Feat: getAnswer function script

--- fake-news-ai/src/App.svelte
+++ fake-news-ai/src/App.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
-  
+  import { Stretch } from "svelte-loading-spinners";
+
+  let loading: string = "hidden";
+  let visibility: string = "hidden";
+  let answer: string = "fake";
+  let percent: string = "0%";
+  let color: string = "#DA2C43";
+  let news: string = "";
+
+  const getAnswer = () => {
+    visibility = "hidden";
+    loading = "unset";
+    fetch("./postrand", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ news }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        answer = data.response;
+        percent = data.percent;
+        answer == "fake" ? (color = "#DA2C43") : (color = "#16d509f2");
+        visibility = "unset";
+        loading = "hidden";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 </script>
 
-<main>
-  Hello
-</main>
+<main>Hello</main>
 
 <style>
-  
 </style>
